e8a0bb2dad982bdf1ff343bda1a4f094cd8f7969
Yoderick Montero <ymontero@gruporead.com>
2024-12-18 16:48:58 -0400

    prisma

--- build/index.js
+++ build/index.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-const database_1 = require("./services/database");
+const client_1 = require("@prisma/client");
 const app = (0, express_1.default)();
+const prisma = new client_1.PrismaClient();
 // Middlewares
 app.use(express_1.default.json());
 dotenv_1.default.config();
@@ -15,5 +16,26 @@ const desiredPort = process.env.PORT || 88;
 app.listen(desiredPort, () => {
     console.log(`server listening in port ${desiredPort}`);
 });
-// Conectar a la base de datos
-(0, database_1.connectToDatabase)();
+const Customer = prisma.customer.create({
+    data: {
+        firstName: 'Yoderick',
+        lastName: 'Montero',
+        email: 'elsa@prisma.io',
+        phone: '809-334-3635',
+        Orders: {
+            create: [
+                {
+                    status: 'First order',
+                    total_amount: 100.50,
+                },
+                {
+                    status: 'Second order',
+                    total_amount: 200.75,
+                },
+            ],
+        },
+    },
+});
+const C = prisma.customer.findMany();
+console.log(C);
+console.log(Customer);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { connectToDatabase } from "./services/database";
+import { PrismaClient } from '@prisma/client';
 const app = express();
-
+const prisma = new PrismaClient();
 
 // Middlewares
 app.use(express.json());
@@ -15,7 +15,30 @@ app.listen(desiredPort, () => {
     console.log(`server listening in port ${desiredPort}`);
 });
 
+const Customer = prisma.customer.create({
+    data: {
+      firstName: 'Yoderick',
+      lastName: 'Montero',
+      email: 'elsa@prisma.io',
+      phone: '809-334-3635',
+      Orders: {
+        create: [
+          {
+            status: 'First order',
+            total_amount: 100.50,
+          },
+          {
+            status: 'Second order',
+            total_amount: 200.75,
+          },
+        ],
+      },
+    },
+  });
+  
+  const C = prisma.customer.findMany()
+  console.log(C);
+  
+  console.log(Customer);
+  
 
-
-// Conectar a la base de datos
-connectToDatabase();
